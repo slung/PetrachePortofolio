a8c48f490f5bc331888fa3033a0416a0a54835cf
slung <slung@maporama.com>
2013-11-13 16:35:05 +0200

    - [ADD] New dynamic words (coloured)

--- src/controllers/LandingPageCtrl.js
+++ src/controllers/LandingPageCtrl.js
@@ -1,6 +1,48 @@
 function LandingPageCtrl($scope, $routeParams, $location) {
+    
+    $scope.mouseOverListen=false;
+    $scope.mouseOverGunControl = false;
+    $scope.mouseOverWebChaser = false;
+    $scope.mouseOverWriting = false;
+    
     $scope.changeView = function (view) {
         $location.path(view);
+    };
+    
+    //Listen
+    $scope.onListenMouseover = function () {
+        $scope.mouseOverListen = true;
+    };
+    
+    $scope.onListenMouseleave = function () {
+        $scope.mouseOverListen = false;
+    };
+    
+    //Gun control
+    $scope.onGunControlMouseover = function () {
+        $scope.mouseOverGunControl = true;
+    };
+    
+    $scope.onGunControlMouseleave = function () {
+        $scope.mouseOverGunControl = false;
+    };
+    
+    //Web chaser
+    $scope.onWebChaserMouseover = function () {
+        $scope.mouseOverWebChaser = true;
+    };
+    
+    $scope.onWebChaserMouseleave = function () {
+        $scope.mouseOverWebChaser = false;
+    };
+    
+    //Writing
+    $scope.onWritingMouseover = function () {
+        $scope.mouseOverWriting = true;
+    };
+    
+    $scope.onWritingMouseleave = function () {
+        $scope.mouseOverWriting = false;
     };
 };
 
