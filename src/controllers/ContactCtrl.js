2c6485b20eec5527aea5c0c2b6f6fe22ec603213
slung <slung@maporama.com>
2013-11-13 18:08:31 +0200

    - [ADD] Contact view

--- src/controllers/ContactCtrl.js
+++ src/controllers/ContactCtrl.js
@@ -1,5 +1,7 @@
-function ContactCtrl($scope) {
-  
+function ContactCtrl($scope, $location) {
+   $scope.changeView = function (view) {
+        $location.path(view);
+    };
 };
 
-myApp.controller('ContactCtrl', ['$scope', ContactCtrl]);
+myApp.controller('ContactCtrl', ['$scope', '$location', ContactCtrl]);
