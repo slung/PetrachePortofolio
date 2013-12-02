88541c4d7da950508718a48686227520de690c22
Silviu Lung <slung@maporama.com>
2013-12-02 09:03:53 +0200

    - [UPDATE] T. Kemper project

--- src/directives.js
+++ src/directives.js
@@ -9,4 +9,19 @@ myApp.directive('projectKeypress', function($document) {
         
         $document.keydown(handler);
     }
-});
+})
+//.directive('footer',['$timeout', function (timer) {
+//    /* Note the injection of the $timeout service */
+//    return {
+//        restrict: 'E',
+//        link: function (scope, elem, attrs, ctrl) {
+//             var replace = function (elem){
+//                var htmlText = 'Copyright &copy Alexandru Petrache ' + new Date().getFullYear() + '. All rights reserved.';
+//                elem.html(htmlText);
+//            }
+//             
+//            timer(replace(elem), 100);
+//        }
+//    }   
+//}]);
+    
